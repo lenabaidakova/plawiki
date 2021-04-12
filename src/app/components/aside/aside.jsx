802b2eed028cd928ff20a1d6d37bd309214bc6d8
e.baidakova <lena.baydakova@gmail.com>
2021-04-12 12:40:40 +0300

    convenient scroll for sidebar

--- src/app/components/aside/aside.jsx
+++ src/app/components/aside/aside.jsx
@@ -4,6 +4,23 @@ import Button from 'app/components/button';
 export default class Aside extends React.Component {
   state = {
     isSidebarHidden: false,
+    scrollTop: 0,
+  };
+
+  componentDidMount() {
+    document.addEventListener('scroll', this.onPageScroll);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('scroll', this.onPageScroll);
+  }
+
+  onPageScroll = () => {
+    const document = window.document.documentElement;
+    const { scrollTop: prevScrollTop } = this.state;
+
+    this.body.scrollTop = this.body.scrollTop + (document.scrollTop - prevScrollTop);
+    this.setState({ scrollTop: document.scrollTop });
   };
 
   onToggleSidebar = () => {
@@ -21,7 +38,10 @@ export default class Aside extends React.Component {
           onClick={this.onToggleSidebar}
         >Show sidebar</Button>
 
-        <div className="aside__body">{children}</div>
+        <div
+          className="aside__body"
+          ref={r => (this.body = r)}
+        >{children}</div>
       </aside>
     );
   }
