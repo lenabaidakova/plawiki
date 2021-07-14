73e43880c38eca17160e759cd8af457911389344
e.baidakova <lena.baydakova@gmail.com>
2021-07-14 10:47:26 +0300

    added lazy loading for routes

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -1,27 +1,28 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { hot } from 'react-hot-loader/root';
 
 import { BrowserRouter, Switch, Redirect, Route } from 'react-router-dom';
 
 import ScrollToTop from 'app/components/scroll-to-top';
 
-import Article from 'app/views/article';
-import Sandbox from 'app/views/sandbox';
-import SearchResult from 'app/views/search-result';
+const Article = lazy(() => import(/* webpackChunkName: "article-route" */ 'app/views/article'));
+const Sandbox = lazy(() => import(/* webpackChunkName: "sandbox-route" */ 'app/views/sandbox'));
+const SearchResult = lazy(() => import(/* webpackChunkName: "search-result-route" */ 'app/views/search-result'));
 
 const AppStates = () => (
   <BrowserRouter>
     <ScrollToTop/>
 
-    <Switch>
-      <Route component={Article} path="/wiki/:title"/>
-      <Route component={SearchResult} path="/search/:title"/>
-      <Route component={Sandbox} path="/sandbox"/>
+    <Suspense fallback={<h1>loading...</h1>}>
+      <Switch>
+        <Route component={Article} path="/wiki/:title"/>
+        <Route component={SearchResult} path="/search/:title"/>
+        <Route component={Sandbox} path="/sandbox"/>
 
-      {/* Redirect to the main page */}
-      <Redirect to="/wiki/Main_Page"/>
-    </Switch>
+        {/* Redirect to the main page */}
+        <Redirect to="/wiki/Main_Page"/>
+      </Switch></Suspense>
   </BrowserRouter>
 );
 
-export default ENV === 'development' ? hot(AppStates) : AppStates;
+export default process.env.NODE_ENV === 'development' ? hot(AppStates) : AppStates;
